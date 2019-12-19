3bc313a064a1919adc3e1e2c87c1fc1fa7e714cf
skulkov <sergey.kulkov@gmail.com>
2019-12-19 11:37:38 +0300

    update config

--- config/config.default.php
+++ config/config.default.php
@@ -1,30 +1,10 @@
 <?php
 
-use WebSK\Skif\Content\ContentType;
-use WebSK\Skif\Content\Template;
-use WebSK\Skif\CRUD\CRUDController;
-
 return [
     'settings' => [
         'displayErrorDetails' => false,
-        'cache' => [
-            'engine' => \WebSK\Cache\Engines\Memcache::class,
-            'cache_key_prefix' => 'skif',
-            'servers' => [
-                [
-                    'host' => 'localhost',
-                    'port' => 11211
-                ]
-            ]
-        ],
         'db' => [
             'db_skif' => [
-                'host' => 'localhost',
-                'db_name' => 'skif',
-                'user' => '',
-                'password' => '',
-            ],
-            'db_logger' => [
                 'host' => 'localhost',
                 'db_name' => 'skif',
                 'user' => '',
@@ -37,26 +17,7 @@ return [
             'blank' => 'layouts/layout.blank.tpl.php',
             'main' => 'layouts/layout.main.tpl.php'
         ],
-        'admin_menu' => [
-            ['link' => '/admin/site_menu', 'name' => 'Менеджер меню', 'icon' => '<i class="fa fa-bars fa-fw"></i>'],
-            ['link' => '/admin/blocks', 'name' => 'Блоки', 'icon' => '<i class="fa fa-table fa-fw"></i>'],
-            ['link' => '/admin/poll', 'name' => 'Опросы', 'icon' => '<i class="fa fa-bar-chart fa-fw"></i>'],
-            ['link' => '/admin/form', 'name' => 'Формы', 'icon' => '<i class="fa fa-list-alt fa-fw"></i>'],
-            ['link' => '/admin/comments', 'name' => 'Комментарии', 'icon' => '<i class="fa fa-comments-o fa-fw"></i>'],
-            ['link' => '/admin/users', 'name' => 'Пользователи', 'icon' => '<i class="fa fa-users fa-fw"></i>'],
-            ['link' => '/admin/rating', 'name' => 'Рейтинги', 'icon' => '<i class="fa fa-star-o fa-fw"></i>'],
-            [
-                'link' => '#',
-                'name' => 'Настройки<span class="fa arrow"></span>',
-                'icon' => '<i class="fa fa-wrench fa-fw"></i>',
-                'sub_menu' => [
-                    ['link' => CRUDController::getListUrl(ContentType::class), 'name' => 'Типы контента'],
-                    ['link' => CRUDController::getListUrl(Template::class), 'name' => 'Темы'],
-                    ['link' => '/admin/redirect', 'name' => 'Редиректы'],
-                    ['link' => '/admin/key_value', 'name' => 'Параметры'],
-                ]
-            ]
-        ]
+
     ],
     'log_path' => '/var/www/log',
     'tmp_path' => '/var/www/tmp',
@@ -68,15 +29,4 @@ return [
     'site_title' => 'WebSK. Skif',
     'site_email' => 'support@websk.ru',
     'salt' => 'webskskif',
-    'content' => [
-        'news' => [
-            'limit_to_page' => 20,
-            'require_main_rubric' => 1,
-            'main_rubric_default_id' => 3
-        ]
-    ],
-    'comments' => [
-        'message_to_page' => 20,
-        'send_answer_to_email' => true
-    ]
 ];
